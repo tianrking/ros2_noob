65c2e4f6d1e8ca981f89ff07bf024184758eecb8
w0x7ce <tian.r.king@gmail.com>
2022-07-31 11:12:37 +0800

    add pub sub

---
+++ cpp_pubsub/build/cpp_pubsub/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/w0x7ce/Downloads/test_ws/src/cpp_pubsub
+# Build directory: /home/w0x7ce/Downloads/test_ws/src/cpp_pubsub/build/cpp_pubsub
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
