b71d90f0f467fcf2d7e259e7bc06b694b55a907a
w0x7ce <tian.r.king@gmail.com>
2022-08-01 22:32:24 +0800

    update

--- pico_micro_ros_example.c
+++ pico_micro_ros_example.c
@@ -5,6 +5,7 @@
 #include <rclc/rclc.h>
 #include <rclc/executor.h>
 #include <std_msgs/msg/int32.h>
+#include <std_msgs/msg/string.h>
 #include <rmw_microros/rmw_microros.h>
 
 #include "pico/stdlib.h"
@@ -19,6 +20,24 @@ void timer_callback(rcl_timer_t *timer, int64_t last_call_time)
 {
     rcl_ret_t ret = rcl_publish(&publisher, &msg, NULL);
     msg.data++;
+}
+
+
+int k = 1;
+void subscription_callback(const void * msgin)
+{
+  // Cast received message to used type
+  const std_msgs__msg__Int32 * msg = (const std_msgs__msg__Int32 *)msgin;
+
+  // Process message
+  printf("Received: %d\n", msg->data);
+  if(k>0){
+        gpio_set_dir(LED_PIN, 1);
+  }
+  else{
+        gpio_set_dir(LED_PIN, 0);
+  }
+  k*= -1;
 }
 
 int main()
@@ -69,6 +88,45 @@ int main()
         &support,
         RCL_MS_TO_NS(1000),
         timer_callback);
+/////////////////
+// Subscription object
+    rcl_subscription_t subscriber;
+    const char * topic_name = "test_topic";
+
+    // Get message type support
+    const rosidl_message_type_support_t * type_support =
+    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32);
+
+    // Initialize a reliable subscriber
+    rcl_ret_t rc = rclc_subscription_init_default(
+    &subscriber, &node,
+    type_support, topic_name);
+
+    if (RCL_RET_OK != rc) {
+    // ...  // Handle error
+    return -1;
+    }
+
+
+    // Message object to receive publisher data
+    std_msgs__msg__Int32 msg;
+
+    // Add subscription to the executor
+    rcl_ret_t rc2 = rclc_executor_add_subscription(
+    &executor, &subscriber, &msg,
+    &subscription_callback, ON_NEW_DATA);
+
+    if (RCL_RET_OK != rc2) {
+    // ...  // Handle error
+    return -1;
+    }
+
+    // Spin executor to receive messages
+    rclc_executor_spin(&executor);
+//////////////////
+
+
+
 
     rclc_executor_init(&executor, &support.context, 1, &allocator);
     rclc_executor_add_timer(&executor, &timer);
