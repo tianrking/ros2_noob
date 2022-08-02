3cbad4620b1bab146f3b34268cffceb428a1b0f4
w0x7ce <tian.r.king@gmail.com>
2022-08-02 22:32:49 +0800

    Add: subscribe

--- pico_micro_ros_example.c
+++ pico_micro_ros_example.c
@@ -14,6 +14,49 @@ const uint LED_PIN = 25;
 
 rcl_publisher_t publisher;
 std_msgs__msg__Int32 msg;
+rcl_subscription_t subscriber;
+rclc_executor_t executor;
+rclc_support_t support;
+rcl_allocator_t allocator;
+rcl_node_t node;
+rcl_timer_t timer;
+
+#define RCCHECK(fn)                  \
+    {                                \
+        rcl_ret_t temp_rc = fn;      \
+        if ((temp_rc != RCL_RET_OK)) \
+        {                            \
+            error_loop();            \
+        }                            \
+    }
+#define RCSOFTCHECK(fn)              \
+    {                                \
+        rcl_ret_t temp_rc = fn;      \
+        if ((temp_rc != RCL_RET_OK)) \
+        {                            \
+        }                            \
+    }
+
+void error_loop()
+{
+    while (1)
+    {
+        // digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+        // delay(100);
+    }
+}
+
+void subscription_callback(const void *msgin)
+{
+    const std_msgs__msg__Int32 *msg = (const std_msgs__msg__Int32 *)msgin;
+    //   digitalWrite(LED_PIN, (msg->data == 0) ? LOW : HIGH);
+    if(msg->data==0){
+        gpio_put(LED_PIN, 1);
+    }
+    else{
+        gpio_put(LED_PIN, 0);
+    }
+}
 
 void timer_callback(rcl_timer_t *timer, int64_t last_call_time)
 {
@@ -24,13 +67,12 @@ void timer_callback(rcl_timer_t *timer, int64_t last_call_time)
 int main()
 {
     rmw_uros_set_custom_transport(
-		true,
-		NULL,
-		pico_serial_transport_open,
-		pico_serial_transport_close,
-		pico_serial_transport_write,
-		pico_serial_transport_read
-	);
+        true,
+        NULL,
+        pico_serial_transport_open,
+        pico_serial_transport_close,
+        pico_serial_transport_write,
+        pico_serial_transport_read);
 
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
@@ -44,7 +86,7 @@ int main()
     allocator = rcl_get_default_allocator();
 
     // Wait for agent successful ping for 2 minutes.
-    const int timeout_ms = 1000; 
+    const int timeout_ms = 1000;
     const uint8_t attempts = 120;
 
     rcl_ret_t ret = rmw_uros_ping_agent(timeout_ms, attempts);
@@ -76,9 +118,30 @@ int main()
     gpio_put(LED_PIN, 1);
 
     msg.data = 0;
+
+    //////////////////////
+    // allocator = rcl_get_default_allocator();
+    // create init_options
+    // RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
+
+    // create node
+    // RCCHECK(rclc_node_init_default(&node, "micro_ros_arduino_node", "", &support));
+
+    // create subscriber
+    RCCHECK(rclc_subscription_init_default(
+        &subscriber,
+        &node,
+        ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+        "micro_ros_arduino_subscriber"));
+
+    // create executor
+    RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
+    RCCHECK(rclc_executor_add_subscription(&executor, &subscriber, &msg, &subscription_callback, ON_NEW_DATA));
+
     while (true)
     {
         rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100));
+        // RCCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100)));
     }
     return 0;
 }
